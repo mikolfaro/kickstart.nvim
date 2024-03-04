70538d4594a4fd554adf0332dbcba5b36425a502
Mikol Faro <mikol.faro@gmail.com>
2024-03-04 21:58:43 +0100

    Add ollama

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,4 +24,35 @@ return {
   { -- Fugitive
     'tpope/vim-fugitive',
   },
+
+  { -- Ollama
+    'nomnivore/ollama.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    cmd = { 'Ollama', 'OllamaModel', 'OllamaServe', 'OllamaServeStop' },
+
+    keys = {
+      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
+      {
+        '<leader>oo',
+        ":<c-u>lua require('ollama').prompt()<cr>",
+        desc = 'ollama prompt',
+        mode = { 'n', 'v' },
+      },
+
+      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+      {
+        '<leader>oG',
+        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
+        desc = 'ollama Generate Code',
+        mode = { 'n', 'v' },
+      },
+    },
+
+    opts = {
+      model = 'wizard-vicuna-uncensored:latest',
+      url = 'http://10.12.14.2:11434',
+    },
+  },
 }
