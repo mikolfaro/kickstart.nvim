5ee07d03da09f0a9df7fd7173d347ed3f9249c17
Mikol Faro <mikol.faro@proton.me>
2024-10-31 12:01:55 +0100

    Add 'parrot' plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,4 +24,19 @@ return {
   { -- Fugitive
     'tpope/vim-fugitive',
   },
+
+  -- { -- Parrot
+  --   'frankroeder/parrot.nvim',
+  --   dependencies = { 'ibhagwan/fzf-lua', 'nvim-lua/plenary.nvim' },
+  --   config = function()
+  --     require("parrot").setup {
+  --       providers = {
+  --         anthropic = {
+  --           api_key = os.getenv("ANTHROPIC_API_KEY"),
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- }
+
 }
