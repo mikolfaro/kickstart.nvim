841c7d5e0acc07ac72b7af9a33033c3bb3b0986f
Mikol Faro <mikol.faro@proton.me>
2025-07-30 16:33:21 +0200

    Move kulala to separated file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -90,21 +90,6 @@ return {
     'sindrets/diffview.nvim'
   },
 
-  { -- Rest and GrapQL client
-    "mistweaverco/kulala.nvim",
-    keys = {
-      { "<leader>Rs", desc = "Send request" },
-      { "<leader>Ra", desc = "Send all requests" },
-      { "<leader>Rb", desc = "Open scratchpad" },
-    },
-    ft = {"http", "rest"},
-    opts = {
-      global_keymaps = false,
-      global_keymaps_prefix = "<leader>R",
-      kulala_keymaps_prefix = "",
-    },
-  },
-
   -- {
   --   dir = "~/workspace_spikes/present.nvim",
   --   config = function ()
---
+++ lua/custom/plugins/kulala.lua
@@ -0,0 +1,16 @@
+return {
+  { -- Rest and GrapQL client
+    "mistweaverco/kulala.nvim",
+    keys = {
+      { "<leader>Rs", desc = "Send request" },
+      { "<leader>Ra", desc = "Send all requests" },
+      { "<leader>Rb", desc = "Open scratchpad" },
+    },
+    ft = {"http", "rest"},
+    opts = {
+      global_keymaps = false,
+      global_keymaps_prefix = "<leader>R",
+      kulala_keymaps_prefix = "",
+    },
+  },
+}
